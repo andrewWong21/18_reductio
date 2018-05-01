e51ed3cf984c99ddba4325e4b872744cff968076
Andrew <awong21@stuy.edu>
2018-05-01 01:14:17 -0400

    finished

--- reductio.py
+++ reductio.py
@@ -5,17 +5,19 @@ text_file.close()
 full_text = full_text.split()
 
 def wordFrequency(target):
- return len([word for word in full_text if word.lower() == target.lower()])
-
+    return len([word for word in full_text if word.lower() == target.lower()])
 
 print "frequency of 'Alice': " + str(wordFrequency("alice"))
 
 def groupFrequency(target_words):
- frequencies = [wordFrequency(word) for word in target_words.split()]
- #print frequencies
- return reduce((lambda a,b : a + b), frequencies)
+    frequencies = [wordFrequency(word) for word in target_words]
+    return reduce((lambda a,b : a + b), frequencies)
 
-print "frequency of 'Alice', 'Rabbit', and 'Cheshire': " + str(groupFrequency("Alice Rabbit Cheshire"))
+print "frequency of 'Alice', 'Rabbit', and 'Cheshire': " + str(groupFrequency(["Alice", "Rabbit", "Cheshire"]))
 
-def mostFrequent():
- 
+def mostFrequent(target_words):
+    frequencies = [[word, wordFrequency(word)] for word in target_words]
+    print frequencies
+    return reduce((lambda a, b : a if a[1] > b[1] else b), frequencies)
+
+print "most frequent word in ['Alice', 'Rabbit', 'Cheshire']: " + str(mostFrequent(["Alice", "Rabbit", "Cheshire"]))
